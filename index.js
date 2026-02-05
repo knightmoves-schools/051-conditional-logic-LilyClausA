86d1b043f687c5715b518399bb0423c8616dd9ec
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-02-05 20:05:41 +0000

    erthyjnu

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function validateAge(oldEnough){
-  // add your code here
+  if (logic){
+    document.getElementById("result").innerHTML = 'old enough';
+  }
 }
+
+
+
+
+//should add logic to the function named validateAge so that it returns "old enough" 
+// if oldEnough is true
+
+//should not return "old enough" from the validateAge function if oldEnough is
+//  false
